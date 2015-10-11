3bfb7dd8ed16a19a8c9840c97a9d4f826f91c2c4
Michał Fabiański <mihal160@gmail.com>
2015-10-11 19:13:52 +0200

    added basic structure

--- src/MP3GUI/PlayerGUI.java
+++ src/MP3GUI/PlayerGUI.java
@@ -1,4 +1,32 @@
 package MP3GUI;
 
+import com.googlecode.lanterna.TerminalFacade;
+import com.googlecode.lanterna.gui.GUIScreen;
+
 public class PlayerGUI {
+    private static PlayerGUI playerGUI = null;
+    public static PlayerGUI getPlayerGUI(){
+        if (playerGUI == null){
+            playerGUI = new PlayerGUI();
+        }
+        return playerGUI;
+    }
+
+    public PlayerGUI(){
+        build();
+    }
+
+    private void build(){
+        GUIScreen textGUI = TerminalFacade.createGUIScreen();
+        if(textGUI == null) {
+            System.err.println("Couldn't allocate a terminal!");
+            return;
+        }
+        textGUI.getScreen().startScreen();
+        textGUI.setTitle("GUI Test");
+
+        //Do GUI logic here
+
+        textGUI.getScreen().stopScreen();
+    }
 }
--- src/Program.java
+++ src/Program.java
@@ -1,5 +1,7 @@
+import MP3GUI.PlayerGUI;
+
 public class Program {
     public static void main(String[] args){
-
+        PlayerGUI gui = PlayerGUI.getPlayerGUI();
     }
 }
